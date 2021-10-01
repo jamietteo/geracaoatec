be8aaffadfacf15e5cde5ca7a1ed6e3cccfa15fe
reacc <luisgoncalvesfc@gmail.com>
2021-10-01 19:35:11 +0100

    sign off feito

--- resources/views/master/header.blade.php
+++ resources/views/master/header.blade.php
@@ -6,10 +6,24 @@
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             @if(Auth::user())
-                <a class="nav-link" href="#">Olá, {{ Auth::user()->name }}!</a>
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    Olá {{ Auth::user()->name }}!
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
             @endif
             @if(!Auth::user())
-                <a class="nav-link" href="{{url('login')}}">Login</a>
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
             @endif
 
         </li>
