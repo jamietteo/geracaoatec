07f8ac483b468a0c1a59df02f2bd68111f9ae17d
Miguel Santos <Miguel.Santos.T0119178@edu.atec.pt>
2021-10-01 22:33:36 +0100

    tables com margem e centrada

--- resources/views/components/tests/tests-list.blade.php
+++ resources/views/components/tests/tests-list.blade.php
@@ -12,7 +12,7 @@
                 </div>
             @endif
 
-            <table class="table table-striped table-bordered">
+            <table class="table table-striped table-bordered m-4 mx-auto">>
                 <thead>
                 <tr>
                     <th scope="col">#</th>
