f48fb2ff65a2cfc7a77633030110eaddf42c9296
Miguel Santos <Miguel.Santos.T0119178@edu.atec.pt>
2021-10-06 18:40:11 +0100

    import no sacss e icone nos butoes de student e groups

--- resources/views/components/students/students-list.blade.php
+++ resources/views/components/students/students-list.blade.php
@@ -1,7 +1,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12 p-4">
-            <h1>Lista de Alunos</h1><a href="{{ url('students/create') }}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Criar aluno</a>
+            <h1>Lista de Alunos</h1><a href="{{ url('students/create') }}" class="btn btn-primary btn-lg active"
+                                       role="button" aria-pressed="true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-plus-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path
+                        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                </svg>
+                Criar aluno</a>
 
             @if ( session('status') )
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -30,12 +38,12 @@
                         <td>{{$student->name}}</td>
                         <td>
                             @if(sizeof($student->groups) > 0)
-                        @foreach($student->groups as $group)
-                            {{$group->name}}
-                        @endforeach
+                                @foreach($student->groups as $group)
+                                    {{$group->name}}
+                                @endforeach
                             @else
                                 Sem Turma
-                                @endif
+                            @endif
                         </td>
                         <td class="text-center align-middle">
                             <div class="pr-1">
