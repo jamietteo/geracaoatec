4bf7259fb2bf0113b4b98f970839c787bcf4307b
miguel santos <miguel_cristinha@hotmail.com>
2021-10-19 02:06:10 +0100

    side bar responsiva em mobile faltava scripts, alterei o web.php pq nao conseguia fazer login comentei uma linha de codigo

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes(['register' => false]);
+Auth::routes();
+//Auth::routes(['register' => false]);
 
 //Login
 Route::get('/', 'HomeController@index')->name('/');
