d5ab04f818a63064574b326f6b47362c3ed84069
reacc <luisgoncalvesfc@gmail.com>
2021-10-21 21:25:36 +0100

    rotas bem feitas e completas, falta adicionar os middlewares

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,7 @@ Route::get('/', 'HomeController@index')->name('/');
 
 //other routes
 Route::view('/home', 'homePage')->middleware('auth');
-Route::view('home', 'homePage')->middleware('auth');
 Route::resource('dashboard', 'HomeController')->middleware('auth');
-Route::resource('groups', 'GroupController')->middleware('auth');
-Route::resource('institutions', 'InstitutionController')->middleware('auth');
-Route::resource('roles', 'RoleController')->middleware('auth');
-Route::resource('sessions', 'SessionController')->middleware('auth');
-Route::resource('students', 'StudentController')->middleware('auth');
-Route::resource('tests', 'TestController')->middleware('auth');
-Route::resource('userForms', 'UserFormController')->middleware('auth');
 
 Route::prefix('users')->group(function(){
     Route::get('', 'UserController@index')->middleware('auth');
@@ -39,7 +31,75 @@ Route::prefix('users')->group(function(){
     Route::delete('{user}', 'UserController@destroy')->middleware('gestor');
 });
 
+Route::prefix('groups')->group(function(){
+    Route::get('', 'GroupController@index')->middleware('auth');
+    Route::get('/create', 'GroupController@create')->middleware('auth');
+    Route::post('', 'GroupController@store')->middleware('auth');
+    Route::get('{group}', 'GroupController@show')->middleware('auth');
+    Route::get('{group}/edit', 'GroupController@edit')->middleware('auth');
+    Route::put('{group}', 'GroupController@update')->middleware('auth');
+    Route::delete('{group}', 'GroupController@destroy')->middleware('gestor');
+});
 
+Route::prefix('institutions')->group(function(){
+    Route::get('', 'InstitutionController@index')->middleware('auth');
+    Route::get('/create', 'InstitutionController@create')->middleware('auth');
+    Route::post('', 'InstitutionController@store')->middleware('auth');
+    Route::get('{institution}', 'InstitutionController@show')->middleware('auth');
+    Route::get('{institution}/edit', 'InstitutionController@edit')->middleware('auth');
+    Route::put('{institution}', 'InstitutionController@update')->middleware('auth');
+    Route::delete('{institution}', 'InstitutionController@destroy')->middleware('gestor');
+});
+
+Route::prefix('roles')->group(function(){
+    Route::get('', 'RoleController@index')->middleware('auth');
+    Route::get('/create', 'RoleController@create')->middleware('auth');
+    Route::post('', 'RoleController@store')->middleware('auth');
+    Route::get('{role}', 'RoleController@show')->middleware('auth');
+    Route::get('{role}/edit', 'RoleController@edit')->middleware('auth');
+    Route::put('{role}', 'RoleController@update')->middleware('auth');
+    Route::delete('{role}', 'RoleController@destroy')->middleware('gestor');
+});
+
+Route::prefix('sessions')->group(function(){
+    Route::get('', 'SessionController@index')->middleware('auth');
+    Route::get('/create', 'SessionController@create')->middleware('auth');
+    Route::post('', 'SessionController@store')->middleware('auth');
+    Route::get('{session}', 'SessionController@show')->middleware('auth');
+    Route::get('{session}/edit', 'SessionController@edit')->middleware('auth');
+    Route::put('{session}', 'SessionController@update')->middleware('auth');
+    Route::delete('{session}', 'SessionController@destroy')->middleware('auth');
+});
+
+Route::prefix('students')->group(function(){
+    Route::get('', 'StudentController@index')->middleware('auth');
+    Route::get('/create', 'StudentController@create')->middleware('auth');
+    Route::post('', 'StudentController@store')->middleware('auth');
+    Route::get('{student}', 'StudentController@show')->middleware('auth');
+    Route::get('{student}/edit', 'StudentController@edit')->middleware('auth');
+    Route::put('{student}', 'StudentController@update')->middleware('auth');
+    Route::delete('{student}', 'StudentController@destroy')->middleware('auth');
+});
+
+Route::prefix('tests')->group(function(){
+    Route::get('', 'TestController@index')->middleware('auth');
+    Route::get('/create', 'TestController@create')->middleware('auth');
+    Route::post('', 'TestController@store')->middleware('auth');
+    Route::get('{test}', 'TestController@show')->middleware('auth');
+    Route::get('{test}/edit', 'TestController@edit')->middleware('auth');
+    Route::put('{test}', 'TestController@update')->middleware('auth');
+    Route::delete('{test}', 'TestController@destroy')->middleware('auth');
+});
+
+Route::prefix('userForms')->group(function(){
+    Route::get('', 'UserFormController@index')->middleware('auth');
+    Route::get('/create', 'UserFormController@create')->middleware('auth');
+    Route::post('', 'UserFormController@store')->middleware('auth');
+    Route::get('{userForm}', 'UserFormController@show')->middleware('auth');
+    Route::get('{userForm}/edit', 'UserFormController@edit')->middleware('auth');
+    Route::put('{userForm}', 'UserFormController@update')->middleware('auth');
+    Route::delete('{userForm}', 'UserFormController@destroy')->middleware('auth');
+});
 
 
 
