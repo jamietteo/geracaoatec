1a936735188d2ec74a11eb2d32c228aa8e63e89c
Luis <luisgoncalvesfc@gmail.com>
2021-10-26 12:33:25 +0100

    graficos feitos!

--- public/js/chart.js
+++ public/js/chart.js
@@ -2,33 +2,35 @@ window.myChartLib = {}
 
 window.myChartLib.chartStudents = (evaluations, medias) =>{
     const labels = [
-        'Nota aluno',
-        'Media Turma',
-        'Nota aluno',
-        'Media Turma',
-        'Nota aluno',
-        'Media Turma',
+        '1º teste',
+        '2º teste',
+        '3º teste',
     ];
+
+    const notas = Object.values(evaluations)
     let soma = [0,0,0]
-    let media = []
     for(let i=0;i<medias.length;i++){
         for(let j=0;j<medias[i].length;j++){
             soma[j] += medias[i][j]
         }
     }
+
     let mediaTurma = []
     for (let i=0;i<medias.length;i++){
         mediaTurma[i] = soma[i]/medias.length
     }
 
-    const notas = Object.values(evaluations)
     const data = {
         labels: labels,
         datasets: [{
-            label: 'Testes',
-            backgroundColor: 'rgb(51, 153, 255)',
-            borderColor: 'rgb(0, 0, 0)',
-            data: [notas[0], media[0], notas[1], media[1], notas[2], media[2]]
+            label : 'Teste Aluno',
+            backgroundColor: ["#468faf","#468faf","#468faf"],
+            data: [notas[0],notas[1],notas[2]]
+        },
+        {
+            label : 'Media Turma',
+            backgroundColor: ["#adb5bd","#adb5bd","#adb5bd"],
+            data: [mediaTurma[0],mediaTurma[1],mediaTurma[2]]
         }]
     };
 
@@ -36,6 +38,7 @@ const config = {
     type: 'bar',
     data: data,
     options: {
+        responsive: true,
         y: {
             min: 0,
             max: 20,
